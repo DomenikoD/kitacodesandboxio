4db0cca7bff1988d6258bb185ac60de34a3cc320
DomenikoD <domeniko.divjak@gmail.com>
2019-01-04 13:50:40 +0100

    mrdanje2

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import HelloWorld from "./kita";
 import "./styles.css";
+import { Component, Prop } from "@stencil/core";
+//import { defineCustomElements } from "test-components/dist/loader";
 
 function App() {
   return (
@@ -9,9 +11,11 @@ function App() {
       <h1>Hello CodeSandbox</h1>
       <h2>mala komponenta!</h2>
       <HelloWorld />
+      <my-first-component name="Max" />
     </div>
   );
 }
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
+//defineCustomElements(window);
